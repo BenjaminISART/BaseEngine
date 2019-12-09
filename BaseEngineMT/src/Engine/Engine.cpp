9e9630f797f776cd367609fe40a1dd586829b987
b.hamelin <b.hamelin@student.isartdigital.com>
2019-12-09 20:17:07 +0100

    can change position of Objects with imgui

--- BaseEngineMT/src/Engine/Engine.cpp
+++ BaseEngineMT/src/Engine/Engine.cpp
@@ -96,34 +96,38 @@ namespace Core
 			// ------
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-			//Renderer::Render::Instance()->RendScene();
-			bool test;
-			ImVec4 test2;
-			{
-				static float f = 0.0f;
-				static int counter = 0;
-
-				
-				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
-				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
-				ImGui::Checkbox("Demo Window", &test);      // Edit bools storing our window open/close state
-				ImGui::Checkbox("Another Window", &test);
-
-				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
-				ImGui::ColorEdit3("clear color", (float*)&test2); // Edit 3 floats representing a color
-
-				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-					counter++;
-				ImGui::SameLine();
-				ImGui::Text("counter = %d", counter);
-
-				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-				ImGui::End();
-			}
 
 			overviewCamera.Update();
 			m_renderer.RendActualScene(s.id);
+			
+			{
+				ImGui::Begin("Scene");                          // Create a window called "Hello, world!" and append into it.
+				for (auto& s : *m_renderer.GetActualScene()->GetSGraph().GetObjects())
+				{
+					ImGui::Text(s.first.c_str());
+
+					ImGui::SliderFloat(std::string(s.first + "pos x").c_str(), &s.second.transform.position.x, -100.0f, 100.0f);
+					ImGui::SliderFloat(std::string(s.first + "pos y").c_str(), &s.second.transform.position.y, -100.0f, 100.0f);
+					ImGui::SliderFloat(std::string(s.first + "pos z").c_str(), &s.second.transform.position.z, -100.0f, 100.0f);
+
+					s.second.transform.UpdateMatrix();
+				}
+				ImGui::End();
+				
+				//ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				//ImGui::Checkbox("Demo Window", &test);      // Edit bools storing our window open/close state
+				//ImGui::Checkbox("Another Window", &test);
+
+				//ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				//ImGui::ColorEdit3("clear color", (float*)&test2); // Edit 3 floats representing a color
+
+				//if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+				//	counter++;
+				//ImGui::SameLine();
+				//ImGui::Text("counter = %d", counter);
+
+				//ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
 
 			ImGui::Render();
 			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
